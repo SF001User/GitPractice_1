6fea53dde2b146fd7c1a351eb9b6ac69dde722a0
B23-User <fedbidtest1@gmail.com>
2021-07-29 14:32:51 -0400

    creating new arraylist

--- src/day3/ListPractice.java
+++ src/day3/ListPractice.java
@@ -1,4 +1,14 @@
 package day3;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ListPractice {
+
+    public static void main(String[] args) {
+
+        List<String> lst = new ArrayList<>();
+
+    }
+
 }
